b4d340c9451ccf18dd1856a1f469a7b5356136ef
choihabin <chabin7181@gmail.com>
2025-06-17 14:33:09 +0900

    fix : 씬을 언로드하거나 Play 모드를 종료하는 시점에 GameObject가 갑자기 생성되는 에러
    - _init == fales인 경우에만 초기화를 하게 해서, 한 번만 실행되도록 보장

--- Assets/@Scripts/Manager/Singleton.cs
+++ Assets/@Scripts/Manager/Singleton.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
+	private static bool _init = false;
 	private static T _instance;
 
 	public static T Instance
 	{
 		get
 		{
-			if (_instance == null)
+			if (_instance == null && _init == false)
 			{
+				_init = true;
 				_instance = (T)GameObject.FindAnyObjectByType(typeof(T));
 
 				if (_instance == null)
